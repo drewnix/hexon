ab3ba222a1db8b6ed6db0816682d903e069a3bb4
Andrew Tanner <andrew@refleqtive.com>
2015-04-22 07:47:30 -0700

    Add full unionfind algorithms

--- unionfind/unionfind.cpp
+++ unionfind/unionfind.cpp
@@ -3,15 +3,68 @@
 #include <string>
 #include <vector>
 
-class QuickFindUF {
-  private:
+
+class QuickUnionUF {
+ private:
     int *id;
-  public:
-    QuickFindUF(int N) {
+    int id_len;
+
+    // fetches the root id by following id[i] up
+    int root(int i) {
+        while(i != id[i]) i = id[i];
+        return i;
+    }
+ public:
+    QuickUnionUF(int N) {
         id = new int[N];
+        id_len = N;
         for (int i = 0; i < N; i++)
             id[i] = i;
+    }
 
+    bool connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    void s_union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+
+};
+
+class QuickFindUF {
+ private:
+    int *id;
+    int id_len;
+ public:
+    QuickFindUF(int N) {
+        id = new int[N];
+        id_len = N;
+        for (int i = 0; i < N; i++)
+            id[i] = i;
+    }
+
+    // (O)n^2 implementation
+    void s_union(int p, int q) {
+        int pid = id[p];
+        int qid = id[q];
+
+        for (int i=0; i<id_len; i++) {
+            if (id[i] == pid)
+                id[i] = qid;
+        }
+    }
+
+    bool connected(int p, int q) {
+        return (id[p] == id[q]);
+    }
+
+    void show() {
+        for (int i=0; i<id_len; i++) {
+            printf("%d: %d\n", i, id[i]);
+        }
     }
 };
 
@@ -23,31 +76,37 @@ vector<T> split(const string& line) {
     return vector<T>(istream_iterator<T>(is), istream_iterator<T>());
 }
 
-
 int main() {
     int N;
     string line;
 
-    QuickFindUF *qf = new QuickFindUF(10);
-
     cout << "Enter number of objects: ";
     cin >> N;
 
+    QuickFindUF *qf = new QuickFindUF(N);
+
+    getline(cin, line);
+
     while (getline(cin, line))
     {
+        if (line.size() == 0)
+            break;
+
         vector<int> vec = split<int>(line);
         int p, q;
 
         if (vec.size() < 2 || vec.size() > 2) {
-            cout << "Error: must specify exacrtly two items.\n" << endl;
+            cout << "Error: must specify exactly two items.\n" << endl;
 
         } else {
             p = vec[0];
             q = vec[1];
 
-            printf("p: %d, q: %d\n", p, q);
+            qf->s_union(p, q);
         }
     }
+
+    qf->show();
 
     return 0;
 }
