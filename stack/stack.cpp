1a4669182514b81d34c80e73cec439dff2dbd50f
Andrew Tanner <andrew@refleqtive.com>
2015-04-24 10:21:06 -0700

    Add full arraystack implementation and cleanup.

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -5,27 +5,79 @@
 
 using namespace std;
 
+class ArrayStack {
+private:
+    string* s;
+    int N = 0;
+    int slength = 16;
+public:
+    ArrayStack() {
+        s = new string[slength];
+    }
+
+    string pop() {
+        string word;
+
+        if (N > 0) {
+            word = s[--N];
+            cout << "Pop: " << word << endl;
+
+            // shrink array when size = 1/4th of total array size
+            if (N < slength/4) {
+                slength = slength/2;
+                string* copy = new string[slength];
+                for(int i=0; i <= N; i++)
+                    copy[i] = s[i];
+
+                // delete/free old array
+                delete [] s;
+                s = copy;
+            }
+        } else {
+            cout << "Empty stack" << endl;
+            word = "";
+        }
+        return  word;
+    }
+
+    void push(string word) {
+        if (N > slength) {
+            // allocate a new array
+            slength = slength*2;
+            string* copy = new string[slength];
+            for(int i=0; i <= N; i++)
+               copy[i] = s[i];
+
+            delete [] s;
+            s = copy;
+
+        } else {
+            s[N++] = word;
+            cout << "N is: " << N << endl;
+        }
+    }
+};
+
 class Node {
 public:
     Node(string new_word) { word = new_word; }
     string word;
     Node* prev;
-    Node* get_prev() { return prev; }
 };
 
-class Stack {
+class LinkedStack {
     Node* last = NULL;
 public:
     string pop() {
         string word;
 
         if (last == NULL) {
-            cout << "Error: empty stack!" << endl;
+            cout << "Empty stack" << endl;
             word = "";
         } else {
             word = last->word;
             last = last->prev;
-            cout << "Last word was: " << word << endl;
+            cout << "Pop: " << word << endl;
         }
         return word;
     }
@@ -44,13 +96,13 @@ public:
     }
 };
 
-
-
 int main() {
+    //LinkedStack* stack = new LinkedStack;
+    ArrayStack* stack = new ArrayStack();
     string word;
-    Stack* stack = new Stack;
 
     while (cin >> word) {
+        // words that are '-' pop the last item put on the stack
         if (word == "-") {
             stack->pop();
         } else {
